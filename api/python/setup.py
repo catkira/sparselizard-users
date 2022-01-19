9ee0350c2e55fd72533901c55a1eca8840db84a2
Benjamin Menkuec <benjamin@menkuec.de>
2022-01-19 18:33:57 +0100

    rebase on upstream

---
+++ api/python/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import os
+
+
+setup(
+    name='spylizard',
+    maintainer="spylizard Developers",
+    maintainer_email="example@python.org",
+    description="PEP 561 type stubs for spylizard",
+    version='1.0',
+    packages=['spylizard'],
+    package_dir={'spylizard':'./package'},
+    include_package_data = True,
+    package_data={"": ["__init__.pyi", "spylizard.so"]},
+)
